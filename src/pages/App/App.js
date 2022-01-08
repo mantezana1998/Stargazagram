dec8f1337e1a41e13ae7bbecb4966ffced5c1675
mantezana <maximo00antezana@gmail.com>
2022-01-07 16:09:50 -0800

    login and signup bug fixed

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import SignupPage from '../SignupPage/SignupPage';
 import LoginPage from '../LoginPage/LoginPage';
-// import Feed from '../Feed';
-// import Layout from '../Layout';
 
 export default function App() {
   return (
--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -3,32 +3,85 @@ import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui
 import '../SignupPage/SignupPage.css'
 
 export default function SignUpPage (){
-  return(
-    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header id='header'>
-        <Image src='https://i.imgur.com/WsX89jL.jpg' /> Login into Star Gazing
-      </Header>
-      <Form>
-        <Segment stacked>
-          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-          <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            type='password'
-          />
 
-          <Button id='button'>
-            Login
-          </Button>
-        </Segment>
-      </Form>
-      <Message id='message'>
+  const [state, setState] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConf: '',
+    bio: ''
+  })
+
+  function handleChange(e){
+    const name = e.target.name
+    const value = e.target.value
+    setState({...setState, [name] : [value]})
+  }
+
+  return (
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+   <Grid.Column style={{ maxWidth: 450 }}>
+     <Header id='header'>
+       <Image src="https://i.imgur.com/WsX89jL.jpg" /> Sign Up for Star Gazing
+     </Header>
+     <Form autoComplete="off">
+       <Segment stacked>
+         <Form.Input
+           name="username"
+           placeholder="username"
+           value={state.username}
+           onChange={handleChange}
+           required
+         />
+         <Form.Input
+           type="email"
+           name="email"
+           placeholder="email"
+           value={state.email}
+           onChange={handleChange}
+           required
+         />
+         <Form.Input
+           name="password"
+           type="password"
+           placeholder="password"
+           value={state.password}
+           onChange={handleChange}
+           required
+         />
+         <Form.Input
+           name="passwordConf"
+           type="password"
+           placeholder="Confirm Password"
+           value={state.passwordConf}
+           onChange={handleChange}
+           required
+         />
+         <Form.TextArea
+           label="bio"
+           name="bio"
+           placeholder="Tell us more about you..."
+           onChange={handleChange}
+         />
+         <Form.Field>
+           <Form.Input
+             type="file"
+             name="photo"
+             placeholder="upload image"
+            //  onChange={handleFileInput}
+           />
+         </Form.Field>
+         <Button type="submit" id='button'>
+           Signup
+         </Button>
+       </Segment>
+       {/* {error ? <ErrorMessage error={error} /> : null} */}
+     </Form>
+     <Message id='message'>
         Already have an account? <a href='/login'>Login</a>
       </Message>
-    </Grid.Column>
-  </Grid>
-  )
+   </Grid.Column>
+ </Grid>
+
+   );
 }
